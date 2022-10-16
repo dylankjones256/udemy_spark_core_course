2498976d38c16d929141613494bed6a263903430
dylankjones256 <113266676+dylankjones256@users.noreply.github.com>
2022-10-16 16:43:06 +0100

    Added includes files

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/databricksstoragetwo/raw"
+processed_folder_path = "/mnt/databricksstoragetwo/processed"
+presentation_folder_path = "/mnt/databricksstoragetwo/presentation"
