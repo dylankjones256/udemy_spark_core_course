1ddd14311cfacd7e91982fff256f029c6b00a6c7
djdillanator@googlemail.com <djdillanator@googlemail.com>
2024-07-26 20:41:07 +0000

    Cleared out old files for course rerun

--- README.md
+++
@@ -1,2 +0,0 @@
-# udemy_spark_core_course
-My files for the 'Azure Databricks &amp; Spark Core For Data Engineers' course on Udemy.
--- configuration.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-raw_folder_path = "/mnt/databricksstoragetwo/raw"
-processed_folder_path = "/mnt/databricksstoragetwo/processed"
-presentation_folder_path = "/mnt/databricksstoragetwo/presentation"
